ee500e1ff3a9411094e8acfe9411eaf6b82490cb
im-allan <al.silvaquiroz@gmail.com>
2024-06-14 16:59:32 -0400

    Feat: connect to db

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,14 @@
+import 'dotenv/config' 
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose.connect(process.env.MONGODB_URI || "")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 
 const app = express();
 app.listen(3000, () => {
