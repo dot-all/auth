7842b605cffb492ac37701c5955b680f4236dd45
im-allan <al.silvaquiroz@gmail.com>
2024-06-16 16:50:32 -0400

    Feat: create test API route

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
-import 'dotenv/config' 
 import express from "express";
 import mongoose from "mongoose";
+import "dotenv/config";
+
+import userRoutes from "./routes/user.route.ts";
 
 mongoose.connect(process.env.MONGODB_URI || "")
   .then(() => {
@@ -14,3 +16,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000.");
 });
+
+app.use("/api/user", userRoutes);
